6da2dafa61563d8f10b3c642fee837e0e444e9c7
faldo <pn4066283@gmail.com>
2024-03-26 14:13:31 +0700

    second commit

--- src/components/NavbarComponents.jsx
+++ src/components/NavbarComponents.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import { menu } from "../data/main";
 // import logo from '../assets/logo.svg'
 import "./css/navbarcomponents.css";
 const NavbarComponents = () => {
+  const [mobileOpen, setMobileOpen] = useState("bx bx-menu");
+  const [listOpen, setListOpen] = useState("nav-list");
+  const [borderOpen, setBorderOpen] = useState("navbar");
+
+  const openThisMenu = () => {
+    mobileOpen === "bx bx-menu"
+      ? setMobileOpen("bx bx-x")
+      : setMobileOpen("bx bx-menu");
+
+    listOpen === "nav-list"
+      ? setListOpen("nav-list active")
+      : setListOpen("nav-list");
+
+    borderOpen === "navbar"
+      ? setBorderOpen("navbar active")
+      : setBorderOpen("navbar");
+  };
+
   return (
     <header>
-      <div className="navbar">
+      <div className={borderOpen}>
         <div className="navbar-logo">
-          <h1>RundWeb<span>.</span></h1>
+          <h1>
+            RundWeb<span>.</span>
+          </h1>
         </div>
-        <ul className="nav-list">
+        <ul className={listOpen}>
           {menu.map((menus) => {
             return (
               <li className="nav-link" key={menus.id}>
@@ -17,6 +38,10 @@ const NavbarComponents = () => {
             );
           })}
         </ul>
+
+        <div className="nav-toggle" onClick={openThisMenu}>
+          <i className={mobileOpen}></i>
+        </div>
       </div>
     </header>
   );
