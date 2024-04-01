d444a5c2786dcfa4f922a471d9738a314c3f18ed
faldo <pn4066283@gmail.com>
2024-04-01 13:40:00 +0700

    services

--- src/pages/ServicesPages.jsx
+++ src/pages/ServicesPages.jsx
@@ -31,9 +31,9 @@ const ServicesPages = () => {
           </div>
           <div className="service-link">
             <div className="service-icon">
-            <i className='bx bx-user-voice'></i>
+              <i className="bx bx-notepad"></i>
             </div>
-            <h1>Website Builder</h1>
+            <h1>UI & UX Design</h1>
             <div className="service-view">
               <a href=""></a>
               <i className="bx bx-right-arrow-alt"></i>
@@ -41,9 +41,9 @@ const ServicesPages = () => {
           </div>
           <div className="service-link">
             <div className="service-icon">
-              <i className="bx bx-laptop"></i>
+              <i className="bx bx-book-content"></i>
             </div>
-            <h1>Website Builder</h1>
+            <h1>Landing Page</h1>
             <div className="service-view">
               <a href=""></a>
               <i className="bx bx-right-arrow-alt"></i>
@@ -51,9 +51,9 @@ const ServicesPages = () => {
           </div>
           <div className="service-link">
             <div className="service-icon">
-              <i className="bx bx-laptop"></i>
+              <i className="bx bx-buildings"></i>
             </div>
-            <h1>Website Builder</h1>
+            <h1>Company Profile</h1>
             <div className="service-view">
               <a href=""></a>
               <i className="bx bx-right-arrow-alt"></i>
