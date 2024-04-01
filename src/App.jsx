fe84bf961497e1f18613becc89a749cd523fce0f
faldo <pn4066283@gmail.com>
2024-04-01 11:35:55 +0700

    first services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavbarComponents from "./components/NavbarComponents";
 import HomePages from "./pages/HomePages";
+import ServicesPages from "./pages/ServicesPages";
 
 function App() {
   return (
     <>
       <NavbarComponents />
       <HomePages/>
+      <ServicesPages/>
     </>
   );
 }
---
+++ src/pages/ServicesPages.jsx
@@ -0,0 +1,68 @@
+import "./css/service.css";
+
+const ServicesPages = () => {
+  return (
+    <section className="service" id="service">
+      <div className="service-content">
+        <div className="service-text">
+          <div className="service-text-left">
+            <h1>
+              Selamat datang dihalaman resmi rundweb<span>.</span>
+            </h1>
+            <p>
+              Kami melayani anda dengan sebuah ide dan gagasan sehingga anda
+              tidak kesulitan dalam membuat situs website impian anda.
+            </p>
+          </div>
+          <div className="service-text-right">
+            <h3>Layanan yang kami sajikan sangat bervariasi</h3>
+          </div>
+        </div>
+        <div className="service-list">
+          <div className="service-link">
+            <div className="service-icon">
+              <i className="bx bx-laptop"></i>
+            </div>
+            <h1>Website Builder</h1>
+            <div className="service-view">
+              <a href=""></a>
+              <i className="bx bx-right-arrow-alt"></i>
+            </div>
+          </div>
+          <div className="service-link">
+            <div className="service-icon">
+            <i className='bx bx-user-voice'></i>
+            </div>
+            <h1>Website Builder</h1>
+            <div className="service-view">
+              <a href=""></a>
+              <i className="bx bx-right-arrow-alt"></i>
+            </div>
+          </div>
+          <div className="service-link">
+            <div className="service-icon">
+              <i className="bx bx-laptop"></i>
+            </div>
+            <h1>Website Builder</h1>
+            <div className="service-view">
+              <a href=""></a>
+              <i className="bx bx-right-arrow-alt"></i>
+            </div>
+          </div>
+          <div className="service-link">
+            <div className="service-icon">
+              <i className="bx bx-laptop"></i>
+            </div>
+            <h1>Website Builder</h1>
+            <div className="service-view">
+              <a href=""></a>
+              <i className="bx bx-right-arrow-alt"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ServicesPages;
