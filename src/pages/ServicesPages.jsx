6df6aaa351112499e88947c645ff0cf99cc787c0
faldo <pn4066283@gmail.com>
2024-04-16 16:25:35 +0700

    fix service

--- src/pages/ServicesPages.jsx
+++ src/pages/ServicesPages.jsx
@@ -14,46 +14,78 @@ const ServicesPages = () => {
               tidak kesulitan dalam membuat situs website impian anda.
             </p>
           </div>
-          <div className="service-text-right" data-aos="fade-left" data-aos-duration="1000">
+          <div
+            className="service-text-right"
+            data-aos="fade-left"
+            data-aos-duration="1000"
+          >
             <h3>Layanan yang kami sajikan sangat bervariasi</h3>
           </div>
         </div>
         <div className="service-list">
-          <div className="service-link" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
-            <div className="service-icon">
-              <i className="bx bx-laptop"></i>
+          <div
+            className="service-link"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="500"
+          >
+            <div className="service-no-aos">
+              <div className="service-icon">
+                <i className="bx bx-laptop"></i>
+              </div>
+              <h1>Website Builder</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+              </p>
             </div>
-            <h1>Website Builder</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
-            </p>
           </div>
-          <div className="service-link" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="700">
-            <div className="service-icon">
-              <i className="bx bx-notepad"></i>
+          <div
+            className="service-link"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="700"
+          >
+            <div className="service-no-aos">
+              <div className="service-icon">
+                <i className="bx bx-notepad"></i>
+              </div>
+              <h1>UI & UX Design</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+              </p>
             </div>
-            <h1>UI & UX Design</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
-            </p>
           </div>
-          <div className="service-link" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="900">
-            <div className="service-icon">
-              <i className="bx bx-book-content"></i>
+          <div
+            className="service-link"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="900"
+          >
+            <div className="service-no-aos">
+              <div className="service-icon">
+                <i className="bx bx-book-content"></i>
+              </div>
+              <h1>Landing Page</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+              </p>
             </div>
-            <h1>Landing Page</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
-            </p>
           </div>
-          <div className="service-link" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="1100">
-            <div className="service-icon">
-              <i className="bx bx-buildings"></i>
+          <div
+            className="service-link"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="1100"
+          >
+            <div className="service-no-aos">
+              <div className="service-icon">
+                <i className="bx bx-buildings"></i>
+              </div>
+              <h1>Company Profile</h1>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+              </p>
             </div>
-            <h1>Company Profile</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
-            </p>
           </div>
         </div>
       </div>
