14f15153e4c871cdcb2bb9a42c3130202e157060
faldo <pn4066283@gmail.com>
2024-04-16 14:44:29 +0700

    services fix

--- src/pages/ServicesPages.jsx
+++ src/pages/ServicesPages.jsx
@@ -24,40 +24,36 @@ const ServicesPages = () => {
               <i className="bx bx-laptop"></i>
             </div>
             <h1>Website Builder</h1>
-            <div className="service-view">
-              <a href=""></a>
-              <i className="bx bx-right-arrow-alt"></i>
-            </div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+            </p>
           </div>
           <div className="service-link">
             <div className="service-icon">
               <i className="bx bx-notepad"></i>
             </div>
             <h1>UI & UX Design</h1>
-            <div className="service-view">
-              <a href=""></a>
-              <i className="bx bx-right-arrow-alt"></i>
-            </div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+            </p>
           </div>
           <div className="service-link">
             <div className="service-icon">
               <i className="bx bx-book-content"></i>
             </div>
             <h1>Landing Page</h1>
-            <div className="service-view">
-              <a href=""></a>
-              <i className="bx bx-right-arrow-alt"></i>
-            </div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+            </p>
           </div>
           <div className="service-link">
             <div className="service-icon">
               <i className="bx bx-buildings"></i>
             </div>
             <h1>Company Profile</h1>
-            <div className="service-view">
-              <a href=""></a>
-              <i className="bx bx-right-arrow-alt"></i>
-            </div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae
+            </p>
           </div>
         </div>
       </div>
