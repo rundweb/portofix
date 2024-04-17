2c63848a2d1ebf4a7e79f58f84a011202b774e62
faldo <pn4066283@gmail.com>
2024-04-17 10:39:18 +0700

    fix oke

--- src/components/FooterComponents.jsx
+++ src/components/FooterComponents.jsx
@@ -3,57 +3,51 @@ import "./css/footercomponents.css";
 const FooterComponents = () => {
   return (
     <div className="footer" id="footer">
-      <div
-        className="footer-content"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      >
-        <div className="footer-logo">
-          <a href="">
-            RundWeb<span>.</span>
-          </a>
-          <div className="footer-icon">
+      <div className="footerrrrr" data-aos="fade-up" data-aos-duration="1000">
+        <div className="footer-content">
+          <div className="footer-logo">
             <a href="">
-              <i className="bx bxl-github"></i>
+              RundWeb<span>.</span>
             </a>
+            <div className="footer-icon">
+              <a href="">
+                <i className="bx bxl-github"></i>
+              </a>
+              <a href="">
+                <i className="bx bxl-dribbble"></i>
+              </a>
+              <a href="">
+                <i className="bx bxl-instagram"></i>
+              </a>
+              <a href="">
+                <i className="bx bxl-meta"></i>
+              </a>
+              <a href="">
+                <i className="bx bxl-tiktok"></i>
+              </a>
+            </div>
+          </div>
+          <div className="footer-contatc">
+            <ul>
+              <li>
+                <h3>Contact Us</h3>
+              </li>
+              <li>example@gmail.com</li>
+              <li>Jawa Barat, Indonesia</li>
+              <li>081234567890</li>
+            </ul>
+          </div>
+          <div className="footer-wa-link">
+            <h3>Your Dream</h3>
+            <p>Jadikan website impianmu menjadi kenyataan bersama RundWeb</p>
             <a href="">
-              <i className="bx bxl-dribbble"></i>
-            </a>
-            <a href="">
-              <i className="bx bxl-instagram"></i>
-            </a>
-            <a href="">
-              <i className="bx bxl-meta"></i>
-            </a>
-            <a href="">
-              <i className="bx bxl-tiktok"></i>
+              Contact Us <i className="bx bxl-whatsapp"></i>
             </a>
           </div>
         </div>
-        <div className="footer-contatc">
-          <ul>
-            <li>
-              <h3>Contact Us</h3>
-            </li>
-            <li>example@gmail.com</li>
-            <li>Jawa Barat, Indonesia</li>
-            <li>081234567890</li>
-          </ul>
+        <div className="footer-bottom">
+          <p>2024 RundWeb Productions. All right reserved</p>
         </div>
-        <div className="footer-wa-link">
-          <h3>Your Dream</h3>
-          <p>Jadikan website impianmu menjadi kenyataan bersama RundWeb</p>
-          <a href="">
-            Contact Us <i className="bx bxl-whatsapp"></i>
-          </a>
-        </div>
-      </div>
-      <div
-        className="footer-bottom"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      >
-        <p>2024 RundWeb Productions. All right reserved</p>
       </div>
     </div>
   );
